a8b8bb7749fc8fc242e0a9522bd5bfacdfcc5556
mainbord <n56016gb@gmail.com>
2017-04-05 02:09:15 +0300

    HW 2  - fourth part, layers. Add MealRestController

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,8 +1,28 @@
 package ru.javawebinar.topjava.web.meal;
 
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.to.MealWithExceed;
+
+import java.util.Collection;
 
 public class MealRestController {
     private MealService service;
+
+    public void save(Meal meal, Integer userId) {
+        service.save(meal, userId);
+    }
+
+    public void delete(int id, Integer userId) {
+        service.delete(id, userId);
+    }
+
+    public Meal get(Integer id, Integer userId) {
+        return service.get(id, userId);
+    }
+
+    public Collection<MealWithExceed> getAll(Integer userId, int calories) {
+        return service.getAll(userId,calories);
+    }
 
 }

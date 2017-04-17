bed9f2adc1fa515166b217bcc578d7ea6c561598
mainbord <n56016gb@gmail.com>
2017-04-17 03:51:46 +0300

    HW3 add start of tests

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -34,3 +34,28 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+DELETE FROM meals;
+DELETE FROM user_roles;
+DELETE FROM users;
+ALTER
+SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password');
+
+INSERT INTO users (name, email, password)
+VALUES ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO user_roles (role, user_id) VALUES
+  ('ROLE_USER', 100000),
+  ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (datetime, description, calories, user_id) VALUES
+  ('2015-05-30 10:00', 'Завтрак', 500, 100000),
+  ('2015-05-30 13:00', 'Обед', 1000, 100000),
+  ('2015-05-30 20:00', 'Ужин', 500, 100000),
+  ('2015-05-31 10:00', 'Завтрак', 1000, 100000),
+  ('2015-05-31 13:00', 'Обед', 500, 100000),
+  ('2015-05-31 13:00', 'Обед', 500, 100001),
+  ('2015-05-31 20:00', 'Ужин', 510, 100000);
--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,8 +3,28 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.matcher.ModelMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
+
 public class MealTestData {
 
-    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
+    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>(
+            (expected, actual) -> expected == actual ||
+                    (Objects.equals(expected.getCalories(), actual.getCalories())
+                            && Objects.equals(expected.getId(), actual.getId())
+                            && Objects.equals(expected.getDateTime(), actual.getDateTime())
+                            && Objects.equals(expected.getDescription(), actual.getDescription())
+                    )
+    );
+
+    public static final int MEAL_ID = 100002;
+    public static final Meal MEAL = new Meal(MEAL_ID, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500);
+    public static final int USER_ID = START_SEQ;
+    public static final int ADMIN_ID = START_SEQ + 1;
+
+
 
 }

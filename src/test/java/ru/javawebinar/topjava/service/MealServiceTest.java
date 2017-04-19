e2a42f86e89c460537c3f6c080282a436595daf6
mainbord <n56016gb@gmail.com>
2017-04-19 11:55:02 +0300

    HW 4 add Rule exception

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,27 +1,32 @@
 package ru.javawebinar.topjava.service;
 
-import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.util.DbPopulator;
-import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Arrays;
 
 import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-/**
- * Created by mainbord on 17.04.17.
- */
 @ContextConfiguration({
         "classpath:spring/spring-app.xml",
         "classpath:spring/spring-db.xml"
 })
 @RunWith(SpringJUnit4ClassRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
 
     static {
@@ -31,45 +36,58 @@ public class MealServiceTest {
     @Autowired
     private MealService service;
 
-    @Autowired
-    private DbPopulator dbPopulator;
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
 
-    @Before
-    public void setUp() throws Exception {
-        dbPopulator.execute();
+    @Test
+    public void testDelete() throws Exception {
+        service.delete(MEAL1_ID, USER_ID);
+        MATCHER.assertCollectionEquals(Arrays.asList(MEAL6, MEAL5, MEAL4, MEAL3, MEAL2), service.getAll(USER_ID));
+    }
 
+    @Test(expected = NotFoundException.class)
+    public void testDeleteNotFound() throws Exception {
+        service.delete(MEAL1_ID, 1);
     }
 
     @Test
-    public void get() throws Exception {
-        System.out.println("de");
-        Meal meal = service.get(MEAL_ID, USER_ID);
-        MATCHER.assertEquals(MEAL, meal);
+    public void testSave() throws Exception {
+        Meal created = getCreated();
+        service.save(created, USER_ID);
+        MATCHER.assertCollectionEquals(Arrays.asList(created, MEAL6, MEAL5, MEAL4, MEAL3, MEAL2, MEAL1), service.getAll(USER_ID));
     }
 
     @Test
-    public void delete() throws Exception {
+    public void testGet() throws Exception {
+        Meal actual = service.get(ADMIN_MEAL_ID, ADMIN_ID);
+        MATCHER.assertEquals(ADMIN_MEAL1, actual);
+    }
 
+    @Test(expected = NotFoundException.class)
+    public void testGetNotFound() throws Exception {
+        service.get(MEAL1_ID, ADMIN_ID);
     }
 
     @Test
-    public void getBetweenDateTimes() throws Exception {
+    public void testUpdate() throws Exception {
+        Meal updated = getUpdated();
+        service.update(updated, USER_ID);
+        MATCHER.assertEquals(updated, service.get(MEAL1_ID, USER_ID));
+    }
 
+    @Test(expected = NotFoundException.class)
+    public void testUpdateNotFound() throws Exception {
+        service.update(MEAL1, ADMIN_ID);
     }
 
     @Test
-    public void getAll() throws Exception {
-
+    public void testGetAll() throws Exception {
+        MATCHER.assertCollectionEquals(MEALS, service.getAll(USER_ID));
     }
 
     @Test
-    public void update() throws Exception {
-
+    public void testGetBetween() throws Exception {
+        MATCHER.assertCollectionEquals(Arrays.asList(MEAL3, MEAL2, MEAL1),
+                service.getBetweenDates(LocalDate.of(2015, Month.MAY, 30), LocalDate.of(2015, Month.MAY, 30), USER_ID));
     }
-
-    @Test
-    public void save() throws Exception {
-
-    }
-
 }

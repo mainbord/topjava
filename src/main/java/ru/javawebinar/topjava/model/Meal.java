53a30d995c9a717de1d9d7bbae4f5607837b2538
mainbord <n56016gb@gmail.com>
2017-04-19 11:34:33 +0300

    HW 4 meal add hibernate annotations

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,19 +1,54 @@
 package ru.javawebinar.topjava.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import org.hibernate.validator.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 /**
  * GKislin
  * 11.01.2015.
  */
+
+@NamedQueries({
+        @NamedQuery(name = Meal.SAVE, query =
+                "UPDATE Meal SET description=:description, calories=:calories, dateTime=:date_time WHERE id=:id AND user.id=:user_id"),
+        @NamedQuery(name = Meal.DELETE, query = "DELETE FROM Meal meal WHERE meal.id=:id AND meal.user.id=:user_id"),
+        @NamedQuery(name = Meal.GET, query = "SELECT meal FROM Meal meal WHERE meal.id =:id AND meal.user.id =:user_id"),
+        @NamedQuery(name = Meal.GET_ALL, query = "SELECT meal FROM Meal meal WHERE meal.user.id=:user_id ORDER BY meal.dateTime DESC"),
+        @NamedQuery(name = Meal.GET_BETWEEN, query =
+                "SELECT meal FROM Meal meal WHERE meal.user.id=:user_id AND meal.dateTime BETWEEN :start_date AND :end_date ORDER BY meal.dateTime DESC"),
+})
+
+@Entity
+@Table(name = "meals")
 public class Meal extends BaseEntity {
-    private final LocalDateTime dateTime;
 
-    private final String description;
+    public static final String SAVE = "Meal.save";
+    public static final String DELETE = "Meal.delete";
+    public static final String GET = "Meal.get";
+    public static final String GET_ALL = "Meal.getAll";
+    public static final String GET_BETWEEN = "Meal.getBetween";
 
-    private final int calories;
+    @Column(name = "date_time", nullable = false)
+    @NotNull
+    private LocalDateTime dateTime;
+
+    @Column(name = "description", nullable = false)
+    @NotBlank
+    private String description;
+
+    @Column(name = "calories", nullable = false)
+    @NotNull
+    private int calories;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    public Meal() {
+    }
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this(null, dateTime, description, calories);
@@ -44,6 +79,26 @@ public class Meal extends BaseEntity {
 
     public LocalTime getTime() {
         return dateTime.toLocalTime();
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
     }
 
     @Override

6c04f5259dbe173d2508e18899cff728f275e29a
mainbord <n56016gb@gmail.com>
2017-05-02 13:53:00 +0300

    HW 05 fix crud

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -25,12 +25,15 @@ public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
     Meal save(Meal meal);
 
     @Override
+    @Transactional
 //    @Query("DELETE FROM Meal m WHERE m.id=:id")
     Meal findOne(Integer id);
 
+    @Transactional
     @Query("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC")
     List<Meal> findAll(@Param("userId") int userId);
 
+    @Transactional
     @Query("SELECT m FROM Meal m WHERE m.user.id=:userId AND m.dateTime BETWEEN :startDate AND :endDate ORDER BY m.dateTime DESC")
     List<Meal> getBetween(@Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate, @Param("userId") int userId);
 }

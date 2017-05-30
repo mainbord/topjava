48df9676ed2ab32ace5149d260b0ac11d0ff601d
mainbord <n56016gb@gmail.com>
2017-05-30 12:24:09 +0300

    8 0 refactoring

--- src/main/java/ru/javawebinar/topjava/to/MealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/to/MealWithExceed.java
@@ -1,9 +1,11 @@
 package ru.javawebinar.topjava.to;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 import java.time.LocalDateTime;
 
+/**
+ * GKislin
+ * 11.01.2015.
+ */
 public class MealWithExceed {
     private final Integer id;
 
@@ -15,11 +17,7 @@ public class MealWithExceed {
 
     private final boolean exceed;
 
-    public MealWithExceed(@JsonProperty("id") Integer id,
-                          @JsonProperty("dateTime") LocalDateTime dateTime,
-                          @JsonProperty("description") String description,
-                          @JsonProperty("calories") int calories,
-                          @JsonProperty("exceed") boolean exceed) {
+    public MealWithExceed(Integer id, LocalDateTime dateTime, String description, int calories, boolean exceed) {
         this.id = id;
         this.dateTime = dateTime;
         this.description = description;
--- src/main/webapp/WEB-INF/jsp/fragments/footer.jsp
+++ src/main/webapp/WEB-INF/jsp/fragments/footer.jsp
@@ -1,8 +1,4 @@
 <%@page contentType="text/html" pageEncoding="UTF-8" %>
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
-
-<div class="footer">
-    <div class="container">
-        <spring:message code="app.footer"/>
-    </div>
-</div>
+<hr>
+<footer><spring:message code="app.footer"/></footer>
